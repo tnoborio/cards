0a9545196f2209c9ca163e331e2ff2a1d40e4ebe
Tokusei Noborio <tokusei@nyampass.com>
2022-10-31 18:56:39 +0900

    call cards api

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Card {
+  name: string;
+}
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [cards, setCards] = useState<Card[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      setCards(await axios.get("/api/cards"));
+    })();
+  }, []);
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      {cards.map((card) => (
+        <div>{card.name}</div>
+      ))}
+    </h1>
+  );
 }
 
 export default App;
